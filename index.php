c2b2395ea61285cd33c143cd472a165376228292
Linus Oleander <linus@oleander.nu>
2010-06-03 00:09:14 +0200

    Lägger till en index-fil, koden är hämtad från 'den-kylige'-inlägg https://www.flashback.org/sp23461696

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php 
+    /* Hantering av konfigurering */
+    class Config {
+        private $values = array();
+        
+        public function Config() {
+            $this->set("run", true);
+        }
+        
+        public function set($name, $value) {
+            $this->values[$name] = $value;
+        }
+        
+        public function get($name) {
+            return $this->values[$name];
+        }
+        
+        public function get_all() {
+            return $this->values;
+        }
+    }
+    
+    $GLOBALS["cfg"] = new Config();
+    
+    function cfg() {
+        return $GLOBALS["cfg"];
+    }
+    
+    /* Konfigueringar */
+    cfg() -> set("titel", "En Titel"); // En konfigurering 
+    
+    $cfg = cfg() -> get_all();
+    
+    /* Exempel på hur man kan hantera konfigueringar.
+        $min_cfg["name"] = "Flashbackarn";
+        $min_cfg["sex"] = "male";
+        $min_cfg["age"] = 12;
+        
+        foreach ($min_cfg as $name => $value) {
+            cfg() -> set($name, $value);
+        }
+    */
+?>
+
+<html>  
+
+<head>  
+    <title><?php echo $cfg["titel"]; ?></title>  
+</head>  
+
+<body>  
+<?php   
+    function isPrime($number)
+    {
+        $sqrt = ceil(sqrt($number));
+        $prime = true;
+        for($i = 2; $i <= $sqrt; $i++)
+        {
+            if(($number % $i) == 0 && $i != $number)
+            {
+                $prime = false;
+                break;
+            }
+        }
+        return ($prime) ? true : false;
+    }
+
+    $text  = "Hej. Idag är det "; 
+    $text2 = "Tiden är "; 
+    $today = date('Y-m-d'); 
+    $time  = date("H:i:s"); 
+    $hemligSumma = array(10,20,30,40,50);
+    $slumpadSida = array('www.flashback.org','http://www.piratpartiet.se/','www.google.se','www.php.net');
+
+    echo $text . $today . "<br />" . $text2 . $time . "<br />" ;
+    echo "Idag skall vi räkna matematik: ";
+    
+    if(isset($_POST['submit'])) 
+    {    
+        $summa = intval($_POST["ma1"])  + intval($_POST["ma2"]);
+            echo $_POST["ma1"] . " + " . $_POST["ma2"] . " är lika med " .$summa. "<br /><br />";
+    
+        if($summa == $hemligSumma[mt_rand(0,4)])
+        {
+            echo "Du lyckades hitta det hemliga nummret du får en länk <br />". $slumpadSida[mt_rand(0,3)];
+        }
+        
+        echo (isPrime($summa)) ? $summa . ' är ett primtal<br/>' : $summa . ' är inte ett primtal<br/>';
+    }
+?>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+<input type="text" name="ma1" /> +
+<input type="text" name="ma2" /><br />
+<input type="submit" name="submit" value="Räkna" />
+</form>
+</body>
+
+</html>
