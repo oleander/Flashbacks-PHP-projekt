fdff95a6dc18c3656c9c2f5ebdb89120d594acfa
Joakim Palm <joakim@joakimpalm.se>
2010-06-04 01:20:54 +0200

    Moved the "math section" of index.php to a separate file. Not linked for now

--- index.php
+++ index.php
@@ -83,43 +83,6 @@
 		</div>
 	</div>
 	<div id="content">
-<?php   
-    function isPrime($number)
-    {
-        $sqrt = ceil(sqrt($number));
-        $prime = true;
-        for($i = 2; $i <= $sqrt; $i++)
-        {
-            if(($number % $i) == 0 && $i != $number)
-            {
-                $prime = false;
-            }
-        }
-        return $prime;
-    }
-	  
-	$hemligSumma = array(10,20,30,40,50);
-    $slumpadSida = array('http://www.flashback.org','http://www.piratpartiet.se/','http://www.google.se','http://www.php.net');
-    echo "Idag skall vi räkna matematik: ";
-    
-    if(isset($_POST['submit'])) 
-    {    
-        $summa = intval($_POST["ma1"])  + intval($_POST["ma2"]);
-            echo $_POST["ma1"] . " + " . $_POST["ma2"] . " är lika med " .$summa. "<br /><br />";
-    
-        if($summa == $hemligSumma[mt_rand(0,4)])
-        {
-            echo "Du lyckades hitta det hemliga nummret du får en länk <br />". $slumpadSida[mt_rand(0,3)] ."<br />\n";
-        }
-        
-        echo (isPrime($summa)) ? $summa . ' är ett primtal<br/>' : $summa . ' är inte ett primtal<br/>';
-    }
-?>
-		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-		<input type="text" name="ma1" /> +
-		<input type="text" name="ma2" /><br />
-		<input type="submit" name="submit" value="Räkna" />
-		</form>
 			<div id="dialog" title="Logga in">
 				<form action="login.php" method="post">
 					<label for="username">Anv.namn: </label><input type="text" name="username" /><br />
