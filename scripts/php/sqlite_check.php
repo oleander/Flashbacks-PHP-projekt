ee6537c28bcadc7b3fcfd3f7d19a0ea0e1cae2bf
Joakim Palm <joakim@joakimpalm.se>
2010-06-04 15:25:54 +0200

    Added sql.php + fb.db.
    By request I added two files (sql.php and fb.db) to show
    SQLite3 in action using PHP. The commented out stuff is for use
    with the PDO abstraction class.

--- scripts/php/sqlite_check.php
+++ scripts/php/sqlite_check.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+if(class_exists('SQLiteDatabase'))
+{
+	echo "You have support for SQLite<br />\n";
+}
+else
+{
+	echo "You lack support for SQLite<br />\n";
+}
+*/
 $dbname='base';
 $base=new SQLiteDatabase($dbname, 0666, $err);
 if ($err)
