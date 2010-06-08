a0608fdd9103c1bbcedcf58132990421b1828c61
Joakim Palm <joakim@joakimpalm.se>
2010-06-08 20:28:34 +0200

    Created inc/passwords.php. Functions for passwords and hashes

---
+++ inc/passwords.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Generate 10 character salt
+ */
+function generateSalt()
+{
+	$possibleValues =
+		'0123456789abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ';
+	$salt = '';
+
+	for($i = 0; $i < 10; $i++)
+	{
+		$salt .= $possibleValues[mt_rand(0, strlen($possibleValues)-1)];	
+	}
+
+	return $salt;
+}
+
+/*
+ * Matches entered password with the hash
+ * in the database.
+ */
+function matchPassword($userPassword, $saltAndHash)
+{
+	$hashValues = preg_split('/\./', $saltAndHash);
+	$salt = $hashValues[0];
+	$hash = $hashValues[1];
+
+	if((md5($salt.$userPassword)) == $hash)
+		return true;
+	else
+		return false;
+}
+
+/*
+ * Takes a password then generates a salt
+ * and creates a hash of password and salt.
+ */
+function generateHash($userPassword)
+{
+	$salt = generateSalt();
+	$result = $salt.".".md5($salt.$userPassword);
+
+	return $result;
+}
+
+?>
