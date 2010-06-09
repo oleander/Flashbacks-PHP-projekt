67a60a6d53fffbc506eb810392850d424f2e8799
Joakim Palm <joakim@joakimpalm.se>
2010-06-09 23:41:54 +0200

    Rewrote the news feed script as it only gave me an error before.
    Since previous version tried to write to a file which I didn't
    give write permission to it just gave me an error. Personally
    believe more in this solution but I could be wrong. :)

--- scripts/php/update_feed.php
+++ scripts/php/update_feed.php
@@ -1,4 +1,47 @@
 <?php
+
+// MySQL support required
+require_once("../../inc/mysql_config.php");
+
+$query = "SELECT * FROM news ORDER BY id DESC LIMIT 0,5"; 
+$result = mysql_query($query);
+
+// Tell the browser this is a rss-feed.
+header("Content-Type: application/rss+xml");
+
+// Following is a way to echo everything as it is
+// until matching tag (in this case EOF)
+// (Called "here notation")
+// Bit of downside it has to have start tag at first
+// column to work which screws indentation a bit.
+echo <<<EOF
+<?xml version='1.0' encoding='utf-8'?>
+<rss version='2.0'>
+	<channel>
+		<title>Flashback Community News Feed</title>
+		<link>http://localhost</link>
+		<description>Flashbacks egna community</description>
+EOF;
+
+while($row = mysql_fetch_array($result))
+{
+	echo <<<EOF
+	
+		<item>
+			<title>$row[title]</title>
+			<link>http://localhost</link>
+			<description>$row[content]</description>
+		</item>
+EOF;
+}
+
+
+echo <<<EOF
+	</channel>
+</rss>
+EOF;
+
+/*
 $myFile = "../../inc/feed.xml";
 $fh = fopen($myFile, 'w') or die("can't open file");
 $stringData = "<?xml version='1.0' encoding='UTF-8'?>\n";
@@ -37,4 +80,5 @@ fwrite($fh, $stringData);
 fclose($fh);
 
 header('Location: ../../inc/feed.xml');
+*/
 ?>
