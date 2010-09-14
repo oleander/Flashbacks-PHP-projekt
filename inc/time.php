9dd4c9466dbd6374828d6a2d2e9b93ae889354bc
Diggan <admin@hempinfo.eu>
2010-09-14 23:34:40 +0200

    Tog bort allt

--- inc/time.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$text  = "Hej. Idag är det "; 
-    $text2 = "Tiden är "; 
-    $today = date('Y-m-d'); 
-    $time  = date("H:i:s"); 
-    echo $today . "<br />" . $time . "<br />" ;
-?>
--- scripts/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-foreach(array_keys($_SESSION) as $key)
-{
-	unset($_SESSION[$key]);
-}
-header('Location: ../../index.php');
-?>
